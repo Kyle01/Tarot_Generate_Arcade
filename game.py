dcf804de404479bdb469e8b8169897122826a64a
Kyle McVeigh <kylemcv@gmail.com>
2024-12-24 10:55:13 -0500

    Bottom display buttons

--- game.py
+++ game.py
@@ -5,10 +5,10 @@ SCREEN_WIDTH = 1024
 SCREEN_HEIGHT = 768
 SCREEN_TITLE = "My first Game"
 DEFAULT_LINE_HEIGHT = 24
-DEFAULT_FONT_SIZE = 8
+DEFAULT_FONT_SIZE = 16
 
 INTRO_TEXT = "Ah, welcome, traveler! I am Mama Nyah, and the spirits have brought you to me for a reason. Sit, relax, and let us see what the universe whispers for you. But first, tell me—what is your intention? What does your heart seek to know, heal, or discover? Speak it, and we will find the truth together."
-
+CATEGORIES = ["Love Life", "Professional Development"]
 class MyGame(arcade.Window):
     """ Main application class. """
 
@@ -25,17 +25,52 @@ class MyGame(arcade.Window):
         """ Render the screen. """
         # Clear the screen
         self.clear()
-        start_x = 0
-        start_y = SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 1.5
         arcade.draw_text(INTRO_TEXT,
-                         start_x,
-                         start_y,
+                         0,
+                         SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 1.5,
                          arcade.color.WHITE,
-                         DEFAULT_FONT_SIZE * 2,
+                         DEFAULT_FONT_SIZE,
                          width=SCREEN_WIDTH,
                          multiline=True,
                          align="center")
-
+        
+        arcade.draw_lrtb_rectangle_filled(100, 
+                                          450, 
+                                          200, 
+                                          100,
+                                          arcade.color.IMPERIAL_BLUE)
+        arcade.draw_lrtb_rectangle_filled(125, 
+                                          425, 
+                                          175, 
+                                          125,
+                                      arcade.color.INCHWORM)
+        
+        arcade.draw_text(CATEGORIES[0],
+                         125,
+                         145,
+                         arcade.color.BLACK,
+                         DEFAULT_FONT_SIZE,
+                         width=250,
+                         align="center")
+        
+        arcade.draw_lrtb_rectangle_filled(600, 
+                                          950, 
+                                          200, 
+                                          100,
+                                          arcade.color.IMPERIAL_BLUE)
+        arcade.draw_lrtb_rectangle_filled(625, 
+                                          925, 
+                                          175, 
+                                          125,
+                                      arcade.color.INCHWORM)
+        
+        arcade.draw_text(CATEGORIES[1],
+                         625,
+                         145,
+                         arcade.color.BLACK,
+                         DEFAULT_FONT_SIZE,
+                         width=300,
+                         align="center")
 
     def on_mouse_press(self, x, y, button, key_modifiers):
         """ Called when the user presses a mouse button. """
