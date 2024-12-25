d7677f2efd3b7f568a25f92b4c2dfd7b28bdc66c
Kyle McVeigh <kylemcv@gmail.com>
2024-12-25 07:40:53 -0500

    Slight refactor to make the on_draw method more simple

--- game.py
+++ game.py
@@ -32,78 +32,10 @@ class MyGame(arcade.Window):
         self.clear()
 
         if self.stage == STAGE[2]:
-            arcade.draw_text("Cards:",
-                            100,
-                            SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 1.5,
-                            arcade.color.WHITE,
-                            DEFAULT_FONT_SIZE,
-                            width=500,
-                            align="left")
-            
-            for i, card in enumerate(self.drawn_cards):
-                arcade.draw_text(f"{i+1}:{str(card)}",
-                            100,
-                            SCREEN_HEIGHT - (i + 1) * 75,
-                            arcade.color.WHITE,
-                            DEFAULT_FONT_SIZE,
-                            width=300,
-                            align="left")
-
-            arcade.draw_text("Fortune:",
-                            100,
-                            400,
-                            arcade.color.WHITE,
-                            DEFAULT_FONT_SIZE,
-                            width=500,
-                            align="left")
-
+            self.__draw_drawn_stage()
         else: 
-            arcade.draw_text(INTRO_TEXT,
-                            0,
-                            SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 1.5,
-                            arcade.color.WHITE,
-                            DEFAULT_FONT_SIZE,
-                            width=SCREEN_WIDTH,
-                            multiline=True,
-                            align="center")
+            self.__draw_intro_stage()
             
-            arcade.draw_lrtb_rectangle_filled(100, 
-                                            450, 
-                                            200, 
-                                            100,
-                                            arcade.color.IMPERIAL_BLUE)
-            arcade.draw_lrtb_rectangle_filled(125, 
-                                            425, 
-                                            175, 
-                                            125,
-                                        arcade.color.INCHWORM)
-            
-            arcade.draw_text(CATEGORIES[0],
-                            125,
-                            145,
-                            arcade.color.BLACK,
-                            DEFAULT_FONT_SIZE,
-                            width=250,
-                            align="center")
-            
-            arcade.draw_lrtb_rectangle_filled(600, 
-                                            950, 
-                                            200, 
-                                            100,
-                                            arcade.color.IMPERIAL_BLUE)
-            arcade.draw_lrtb_rectangle_filled(625, 
-                                            925, 
-                                            175, 
-                                            125,
-                                        arcade.color.INCHWORM)
-            
-            arcade.draw_text(CATEGORIES[1],
-                            625,
-                            145,
-                            arcade.color.BLACK,
-                            DEFAULT_FONT_SIZE,
-                            width=300,
-                            align="center")
 
     def on_mouse_press(self, x, y, _button, _key_modifiers):
         if self.stage == STAGE[0] and x > 100 and x < 450 and y > 100 and y < 200:
@@ -120,6 +52,80 @@ class MyGame(arcade.Window):
         deck = TarotDeck()
         deck.shuffle()
         self.drawn_cards = deck.draw(3)
+
+    def __draw_intro_stage(self):
+        arcade.draw_text(INTRO_TEXT,
+                            0,
+                            SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 1.5,
+                            arcade.color.WHITE,
+                            DEFAULT_FONT_SIZE,
+                            width=SCREEN_WIDTH,
+                            multiline=True,
+                            align="center")
+            
+        arcade.draw_lrtb_rectangle_filled(100, 
+                                        450, 
+                                        200, 
+                                        100,
+                                        arcade.color.IMPERIAL_BLUE)
+        arcade.draw_lrtb_rectangle_filled(125, 
+                                        425, 
+                                        175, 
+                                        125,
+                                    arcade.color.INCHWORM)
+        
+        arcade.draw_text(CATEGORIES[0],
+                        125,
+                        145,
+                        arcade.color.BLACK,
+                        DEFAULT_FONT_SIZE,
+                        width=250,
+                        align="center")
+        
+        arcade.draw_lrtb_rectangle_filled(600, 
+                                        950, 
+                                        200, 
+                                        100,
+                                        arcade.color.IMPERIAL_BLUE)
+        arcade.draw_lrtb_rectangle_filled(625, 
+                                        925, 
+                                        175, 
+                                        125,
+                                    arcade.color.INCHWORM)
+        
+        arcade.draw_text(CATEGORIES[1],
+                        625,
+                        145,
+                        arcade.color.BLACK,
+                        DEFAULT_FONT_SIZE,
+                        width=300,
+                        align="center")
+
+    def __draw_drawn_stage(self):
+        arcade.draw_text("Cards:",
+                100,
+                SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 1.5,
+                arcade.color.WHITE,
+                DEFAULT_FONT_SIZE,
+                width=500,
+                align="left")
+            
+        for i, card in enumerate(self.drawn_cards):
+            arcade.draw_text(f"{i+1}:{str(card)}",
+                        100,
+                        SCREEN_HEIGHT - (i + 1) * 75,
+                        arcade.color.WHITE,
+                        DEFAULT_FONT_SIZE,
+                        width=300,
+                        align="left")
+
+        arcade.draw_text("Fortune:",
+                        100,
+                        400,
+                        arcade.color.WHITE,
+                        DEFAULT_FONT_SIZE,
+                        width=500,
+                        align="left")
 
 
 def main():
