668e0fcb24d7d110095f3e4b6bbc66a6546103e2
Devin Reid <dreid97@gmail.com>
2025-01-17 18:45:21 -0600

    Added cards to popup, changed position

--- game.py
+++ game.py
@@ -346,7 +346,7 @@ class TarotGame(arcade.Window):
                     )
                     button_text = "Begin Reading"
 
-            arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, 75, 350, 100, button_texture)
+            arcade.draw_texture_rectangle(SCREEN_WIDTH // 2, 75, 350, 200, button_texture)
 
             arcade.draw_text(
                     button_text,
@@ -358,6 +358,31 @@ class TarotGame(arcade.Window):
                     align="center",
                     font_name="Old School Adventures"
                 )
+            
+            arcade.draw_text(
+                f"{self.current_revealed_card.name}",
+                SCREEN_WIDTH // 2,
+                SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 4.5,
+                arcade.color.WHITE,
+                DEFAULT_FONT_SIZE * 1.8,
+                width=SCREEN_WIDTH,
+                align="center",
+                anchor_x="center",
+                font_name="Old School Adventures"
+            )
+
+            if len(self.selected_cards) >1 and self.selected_cards[1]:
+
+                x = 300 
+                y = 200  
+                self.selected_cards[0].paint(x, y, show_front=True, is_small=True)
+            
+            if len(self.selected_cards) > 2 and self.selected_cards[2]:
+
+                x=975
+                y=200
+                self.selected_cards[1].paint(x,y,show_front=True, is_small = True)
+
             return  # Stop drawing the rest of the stage while reveal is active
 
 
@@ -365,12 +390,13 @@ class TarotGame(arcade.Window):
         arcade.draw_text(
             "Choose 3 Cards:",
             SCREEN_WIDTH // 2,
-            SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 2,
+            SCREEN_HEIGHT - DEFAULT_LINE_HEIGHT * 4.5,
             arcade.color.WHITE,
-            DEFAULT_FONT_SIZE,
+            DEFAULT_FONT_SIZE *1.5,
             width=SCREEN_WIDTH,
             align="center",
-            anchor_x="center"
+            anchor_x="center",
+            font_name="Old School Adventures"
         )
 
         # Draw the cards
@@ -382,7 +408,7 @@ class TarotGame(arcade.Window):
 
         # Draw previously selected cards in the left corner
         for i, card in enumerate(self.selected_cards):
-            x = 300 + (i * 200)  # Spaced out horizontally with 150 pixels between cards
+            x = 300 + (i * 675)  # Spaced out horizontally with 150 pixels between cards
             y = 200  # Fixed y-coordinate for all cards
             card.paint(x, y, show_front=True, is_small=True)
 
