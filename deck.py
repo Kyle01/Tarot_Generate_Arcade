dc9680ec8f09033f533e6e562948c4317b13fd4b
Devin Reid <dreid97@gmail.com>
2025-01-31 12:20:20 -0600

    Added card borders, changed card saturation, and adjusted

--- deck.py
+++ deck.py
@@ -10,7 +10,7 @@ copyright_asset_path = "assets/copyright"
 backup_assets_path = "assets/riderWeightPublicDomain/convertedx2"
 current_asset_path = None
 current_card_back_path = None
-copyright_card_back_path = "backing_diamond_2x.png"
+copyright_card_back_path = "backing_diamond_2x.1.png"
 backup_card_back_path = "CardBacks.png"
 
 if os.path.isdir(copyright_asset_path):
@@ -44,7 +44,7 @@ class Card:
     def reverse(self):
         self.position = positions[1]
 
-    def paint(self, x, y, show_front, is_hovered = False, scale =1.8, is_small = False, angle = 0):
+    def paint(self, x, y, show_front, is_hovered = False, scale =.9, is_small = False, angle = 0):
         self.x = x
         self.y = y
         if is_small:
@@ -90,87 +90,97 @@ class TarotDeck:
         
 
         if os.path.isdir(copyright_asset_path):
-                self.cards = [
-                Card("The Fool", "major0_fool.png"),
-                Card("The Magician", "major1_magician.png"),
-                Card("The High Priestess", "major2_priestess.png"),
-                Card("The Empress", "major3_empress.png"),
-                Card("The Emperor", "major4_emperor.png"),
-                Card("The Hierophant", "major5_hierophant.png"),
-                Card("The Lovers", "major6_lovers.png"),
-                Card("The Chariot", "major7_chariot.png"),
-                Card("Strength", "major8_strength.png"),
-                Card("The Hermit", "major9_hermit.png"),
-                Card("Wheel of Fortune", "major10_wheel.png"),
-                Card("Justice", "major11_justice.png"),
-                Card("The Hanged Man", "major12_hanged.png"),
-                Card("Death", "major13_death.png"),
-                Card("Temperance", "major14_temperance.png"),
-                Card("The Devil", "major15_devil.png"),
-                Card("The Tower", "major16_tower.png"),
-                Card("The Star", "major17_star.png"),
-                Card("The Moon", "major18_moon.png"),
-                Card("The Sun", "major18_sun.png"),
-                Card("Judgement", "major20_judgement.png"),
-                Card("The World", "major21_world.png"),
-                Card("Ace of Cups", "cups1.png"),
-                Card("Two of Cups", "cups2.png"),
-                Card("Three of Cups", "cups3.png"),
-                Card("Four of Cups", "cups4.png"),
-                Card("Five of Cups", "cups5.png"),
-                Card("Six of Cups", "cups6.png"),
-                Card("Seven of Cups", "cups7.png"),
-                Card("Eight of Cups", "cups8.png"),
-                Card("Nine of Cups", "cups9.png"),
-                Card("Ten of Cups", "cups10.png"),
-                Card("Page of Cups", "cupsP.png"),
-                Card("Knight of Cups", "cupsKn.png"),
-                Card("Queen of Cups", "cupsQ.png"),
-                Card("King of Cups", "cupsK.png"),
-                Card("Ace of Pentacles", "pentacles1.png"),
-                Card("Two of Pentacles", "pentacles2.png"),
-                Card("Three of Pentacles", "pentacles3.png"),
-                Card("Four of Pentacles", "pentacles4.png"),
-                Card("Five of Pentacles", "pentacles5.png"),
-                Card("Six of Pentacles", "pentacles6.png"),
-                Card("Seven of Pentacles", "pentacles7.png"),
-                Card("Eight of Pentacles", "pentacles8.png"),
-                Card("Nine of Pentacles", "pentacles9.png"),
-                Card("Ten of Pentacles", "pentacles10.png"),
-                Card("Page of Pentacles", "pentaclesP.png"),
-                Card("Knight of Pentacles", "pentaclesKn.png"),
-                Card("Queen of Pentacles", "pentaclesQ.png"),
-                Card("King of Pentacles", "pentaclesK.png"),
-                Card("Ace of Swords", "swords1.png"),
-                Card("Two of Swords", "swords2.png"),
-                Card("Three of Swords", "swords3.png"),
-                Card("Four of Swords", "swords4.png"),
-                Card("Five of Swords", "swords5.png"),
-                Card("Six of Swords", "swords6.png"),
-                Card("Seven of Swords", "swords7.png"),
-                Card("Eight of Swords", "swords8.png"),
-                Card("Nine of Swords", "swords9.png"),
-                Card("Ten of Swords", "swords10.png"),
-                Card("Page of Swords", "swordsP.png"),
-                Card("Knight of Swords", "swordsKn.png"),
-                Card("Queen of Swords", "swordsQ.png"),
-                Card("King of Swords", "swordsK.png"),
-                Card("Ace of Wands", "wands1.png"),
-                Card("Two of Wands", "wands2.png"),
-                Card("Three of Wands", "wands3.png"),
-                Card("Four of Wands", "wands4.png"),
-                Card("Five of Wands", "wands5.png"),
-                Card("Six of Wands", "wands6.png"),
-                Card("Seven of Wands", "wands7.png"),
-                Card("Eight of Wands", "wands8.png"),
-                Card("Nine of Wands", "wands9.png"),
-                Card("Ten of Wands", "wands10.png"),
-                Card("Page of Wands", "wandsP.png"),
-                Card("Knight of Wands", "wandsKn.png"),
-                Card("Queen of Wands", "wandsQ.png"),
-                Card("King of Wands", "wandsK.png")
-            ]
-                self.card_back = Card("Card Back", "backing_diamond_2x.png")
+                self.cards = cards = [
+    # Major Arcana
+                Card("The Fool", "major0_fool.1.png"),
+                Card("The Magician", "major1_magician.1.png"),
+                Card("The High Priestess", "major2_priestess.1.png"),
+                Card("The Empress", "major3_empress.1.png"),
+                Card("The Emperor", "major4_emperor.1.png"),
+                Card("The Hierophant", "major5_hierophant.1.png"),
+                Card("The Lovers", "major6_lovers.1.png"),
+                Card("The Chariot", "major7_chariot.1.png"),
+                Card("Strength", "major8_strength.1.png"),
+                Card("The Hermit", "major9_hermit.1.png"),
+                Card("Wheel of Fortune", "major10_wheel.1.png"),
+                Card("Justice", "major11_justice.1.png"),
+                Card("The Hanged Man", "major12_hanged.1.png"),
+                Card("Death", "major13_death.1.png"),
+                Card("Temperance", "major14_temperance.1.png"),
+                Card("The Devil", "major15_devil.1.png"),
+                Card("The Tower", "major16_tower.1.png"),
+                Card("The Star", "major17_star.1.png"),
+                Card("The Moon", "major18_moon.1.png"),
+                Card("The Sun", "major19_sun.1.png"),  
+                Card("Judgement", "major20_judgement.1.png"),
+                Card("The World", "major21_world.1.png"),
+
+                # Cups
+                Card("Ace of Cups", "cups1.1.png"),
+                Card("Two of Cups", "cups2.1.png"),
+                Card("Three of Cups", "cups3.1.png"),
+                Card("Four of Cups", "cups4.1.png"),
+                Card("Five of Cups", "cups5.1.png"),
+                Card("Six of Cups", "cups6.1.png"),
+                Card("Seven of Cups", "cups7.1.png"),
+                Card("Eight of Cups", "cups8.1.png"),
+                Card("Nine of Cups", "cups9.1.png"),
+                Card("Ten of Cups", "cups10.1.png"),
+                Card("Page of Cups", "cupsP.1.png"),
+                Card("Knight of Cups", "cupsKn.1.png"),
+                Card("Queen of Cups", "cupsQ.1.png"),
+                Card("King of Cups", "cupsK.1.png"),
+
+                # Pentacles
+                Card("Ace of Pentacles", "pentacles1.1.png"),
+                Card("Two of Pentacles", "pentacles2.1.png"),
+                Card("Three of Pentacles", "pentacles3.1.png"),
+                Card("Four of Pentacles", "pentacles4.1.png"),
+                Card("Five of Pentacles", "pentacles5.1.png"),
+                Card("Six of Pentacles", "pentacles6.1.png"),
+                Card("Seven of Pentacles", "pentacles7.1.png"),
+                Card("Eight of Pentacles", "pentacles8.1.png"),
+                Card("Nine of Pentacles", "pentacles9.1.png"),
+                Card("Ten of Pentacles", "pentacles10.1.png"),
+                Card("Page of Pentacles", "pentaclesP.1.png"),
+                Card("Knight of Pentacles", "pentaclesKn.1.png"),
+                Card("Queen of Pentacles", "pentaclesQ.1.png"),
+                Card("King of Pentacles", "pentaclesK.1.png"),
+
+                # Swords
+                Card("Ace of Swords", "swords1.1.png"),
+                Card("Two of Swords", "swords2.1.png"),
+                Card("Three of Swords", "swords3.1.png"),
+                Card("Four of Swords", "swords4.1.png"),
+                Card("Five of Swords", "swords5.1.png"),
+                Card("Six of Swords", "swords6.1.png"),
+                Card("Seven of Swords", "swords7.1.png"),
+                Card("Eight of Swords", "swords8.1.png"),
+                Card("Nine of Swords", "swords9.1.png"),
+                Card("Ten of Swords", "swords10.1.png"),
+                Card("Page of Swords", "swordsP.1.png"),
+                Card("Knight of Swords", "swordsKn.1.png"),
+                Card("Queen of Swords", "swordsQ.1.png"),
+                Card("King of Swords", "swordsK.1.png"),
+
+                # Wands
+                Card("Ace of Wands", "wands1.1.png"),
+                Card("Two of Wands", "wands2.1.png"),
+                Card("Three of Wands", "wands3.1.png"),
+                Card("Four of Wands", "wands4.1.png"),
+                Card("Five of Wands", "wands5.1.png"),
+                Card("Six of Wands", "wands6.1.png"),
+                Card("Seven of Wands", "wands7.1.png"),
+                Card("Eight of Wands", "wands8.1.png"),
+                Card("Nine of Wands", "wands9.1.png"),
+                Card("Ten of Wands", "wands10.1.png"),
+                Card("Page of Wands", "wandsP.1.png"),
+                Card("Knight of Wands", "wandsKn.1.png"),
+                Card("Queen of Wands", "wandsQ.1.png"),
+                Card("King of Wands", "wandsK.1.png")
+                ]
+
+                self.card_back = Card("Card Back", "backing_diamond_2x.1.png")
         else: 
             self.cards = [
                 # Major Arcana
